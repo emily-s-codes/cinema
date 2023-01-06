b747f81baf44df337123edc59c19428d004e1596
emilysupercode <sheil.emily.sc@gmail.com>
2023-01-06 16:01:03 +0100

    email to owner functionality added

--- backend_cinema/app.js
+++ backend_cinema/app.js
@@ -3,8 +3,8 @@ import cors from 'cors'
 import morgan from 'morgan'
 import fs from 'fs'
 import nodemailer from 'nodemailer'
-import "./config/config.js"
 import { uid } from 'uid'
+import "./config/config.js"
 
 const PORT = process.env.PORT
 const app = express()
@@ -41,6 +41,7 @@ app.get('/empty', (req, res) => {
 app.put('/reserve/:seat', (req, res) => {
     const reserved = req.body.reserved
     const seat = req.params.seat
+    // const resID = req.body.resID
 
     fs.readFile('./data.json', (err, data) => {
         if (err) {
@@ -52,6 +53,7 @@ app.put('/reserve/:seat', (req, res) => {
 
         if (reserved !== undefined) {
             reservations[index].reserved = reserved
+            // resID = uid()
         }
 
         fs.writeFile('./data.json', JSON.stringify(reservations), (err) => {
@@ -84,7 +86,31 @@ app.delete('/reservations', (req, res) => {
         console.log('post-delete', reservations)
         return res.json(reservations)
     })
+})
 
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: process.env.NODEMAILER_USER,
+        pass: process.env.NODEMAILER_PASSWORD
+    }
+});
+
+app.post('/api/ownermail', (req, res) => {
+    const message = {
+        from: 'thisproject@me.com',
+        to: 'owner@cinema.com',
+        subject: 'new reservation received',
+        text: `Hallo du wunderbarer Mensch, du hast gerade wieder ${req.body.price} Umsatz gemacht.Lass die Korken knallen.`,
+        html: `<p style="color:purple;">${req.body.text}</p>`
+    }
+
+    transport.sendMail(message, (err, info) => {
+        console.log(info)
+        if (err) return res.status(500).json({ message: err })
+        return res.status(200).json({ message: 'good to go' })
+    })
 })
 
 app.listen(PORT, () => console.log('running on port', PORT))
